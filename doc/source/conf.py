8330f314d28cfe56fdb42608504461258691ff06
bfairburn-phx <brandon.fairburn@ansys.com>
2022-03-17 10:15:17 -0400

    attempt to fix "Build HTML" action

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,7 +6,7 @@ from pyansys_sphinx_theme import pyansys_logo_black
 from ansys.product.library import __version__
 
 # Project information
-project = 'ansys-product-library'
+project = 'ansys-modelcenter-api'
 copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
 author = "Ansys Inc."
 release = version = __version__
