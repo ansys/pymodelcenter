535ee747f649de3d9d2e4569066b9872e28545aa
unknown <nathan.sharp@ansys.com>
2022-04-22 14:36:47 -0400

    Added comments. Fixed potential race condition and simplified threading. Removed old .version property in favor of the new get_server_info call.

--- ansys/modelcenter/desktop/main.py
+++ ansys/modelcenter/desktop/main.py
@@ -1,4 +1,6 @@
 # Copyright (c) 2022, Ansys Inc. Unauthorised use, distribution or duplication is prohibited
+# You should not use trio import anywhere else. The library should only depend on anyio. This test function should
+# be removed in favor of anyio unit tests later
 import trio
 from ansys.modelcenter.desktop import ModelCenter
 from ansys.common.variableinterop import *
@@ -7,10 +9,9 @@ from ansys.engineeringworkflow.api import *
 async def main():
     instance = ModelCenter()
     await instance.initialize()
-    version = instance.version
-    print("ModelCenter version: " + version)
     print(await instance.get_server_info())
     
+    # TODO: Hardcoded paths
     wf = await instance.load_workflow("c:\\Users\\nsharp\\Desktop\\delme.pxcz")
     print(await wf.run({"Model.QuickWrap5.a": VariableState(True, RealValue(3.2))}, False, ["Model.QuickWrap5.b"]))
     instance = None
--- ansys/modelcenter/desktop/modelcenter.py
+++ ansys/modelcenter/desktop/modelcenter.py
@@ -20,9 +20,10 @@ class ModelCenter(IDesktopWorkflowEngine):
     """
 
     def __init__(self):
+        # Use a ThreadPoolExecutor to proxy all COM calls onto safe threads so that blocking com calls don't mess up our async/await-ness
+        # Technically max_workers could be 1, but in reality someone will probably want to load multiple workflows at once?
         self._thread = ThreadPoolExecutor(max_workers=4, initializer=lambda: pythoncom.CoInitializeEx(pythoncom.COINIT_MULTITHREADED))
         self._instance =  None
-        self._version = None
         
     # TODO: Error if not initialized from all other methods
     
@@ -30,63 +31,49 @@ class ModelCenter(IDesktopWorkflowEngine):
         # TODO: Error if called twice
         def impl() -> None:
             self._instance = mclib.Application()            
-            # self._instance = mclib.Application()
-            # comclient.Dispatch("ModelCenter.Application") # mclib.IModelCenter(comclient.Dispatch(mclib.Application.CLSID))
+            
+            # Other ways to instantiate MC that didn't quite work right. The above gives you a proper typed object.
+            # NOTE that pythoncom will fall-back on late-binding if you aren't super careful and the call semantics 
+            # for some properties is different between late and eary binding.
+            # comclient.Dispatch("ModelCenter.Application") 
+            # mclib.IModelCenter(comclient.Dispatch(mclib.Application.CLSID))
             # print(self._instance)
-            version = {
-                "major": self._instance.version(0),
-                "minor": self._instance.version(1),
-                "patch": self._instance.version(2)
-            }
-            self._version = Template("${major}.${minor}.${patch}").safe_substitute(version)
             
         await self._to_thread(impl)
 
-    # TODO: Do we need version property with the get_server_info call?
-
-    @property
-    def version(self) -> str:
-        """
-        The version of the ModelCenter Desktop application being used.
-        ModelCenter versions are in the form ``1.2.3`` where
-
-        * -1 is the major version,
-        * -2 is the minor version, and
-        * -3 is the patch version
-        """
-        return self._version
-
     async def _to_thread(self, func, *args, **kwargs) -> Any:
+        """Helper that proxies all calls to a multi-threaded worker thread with anyio async on this side"""
+        # Create a blocking portal so we can signal back to our thread
         async with anyio.from_thread.BlockingPortal() as portal:
-            def _thread_impl():
-                try:
-                    return func(*args, **kwargs)
-                finally:
-                    portal.start_task_soon(portal.stop)
-            future = self._thread.submit(_thread_impl)
+            future = self._thread.submit(func, *args, **kwargs)
+            # portal.stop() is an async method. It isn't documented whether it is safe to call it
+            # from other threads, but since those other threads aren't the async/await thread, you'll
+            # get a warning if you do call it from elsewhere. So proxy the call back to us.
+            future.add_done_callback(lambda f: portal.start_task_soon(portal.stop))
             await portal.sleep_until_stopped()
             return future.result()
 
     async def get_server_info(self) -> DesktopWorkflowEngineInfo:
         def impl() -> DesktopWorkflowEngineInfo:
-            # TODO: Get beta flags out?
-            return DesktopWorkflowEngineInfo(self._instance.version(0), self._instance.version(1), 
-                self._instance.version(2), True, "", self.version, "ModelCenter", 
+            # TODO: Get beta flags out
+            maj = self._instance.version(0)
+            min = self._instance.version(1)
+            patch = self._instance.version(2)
+            return DesktopWorkflowEngineInfo(maj, min, 
+                patch, True, "", 
+                f"{maj}.{min}.{patch}", "ModelCenter", 
                 self._instance.getModelCenterPath())
         return await self._to_thread(impl)
                 
     async def load_workflow(self, file_name: Union[PathLike, str]) -> IWorkflowInstance:
-        # snot = self._instance.parallelInstance()
-        # print(isinstance(snot, comclient.DispatchBaseClass))
-        #test = self._instance.parallelInstance()._oleobj_.QueryInterface(mclib.IModelCenter.CLSID, pythoncom.IID_IDispatch)
-        #print(test)
-        #print(test.InvokeTypes(34, 0x0, 2, (3, 0), ((3, 0),),0))
-        # bla = mclib.IModelCenter(snot._oleobj_)
-        #print(bla)
-        #print(bla._oleobj_)
-        # print(bla.version(0))
-        
+        # TODO: implementing this by using IModelCenter.parallelInstance() instead of limiting the user
+        # to a single open workflow. Might not be needed if we just want to error if multiple workflows
+        # are loaded. Doing this has implications on the UI functions.
         def impl() -> IWorkflowInstance:
+            # parallelInstance() returns a plain IDispatch. We want an IModelCenter. I tried
+            # LOTS of ways and this was the only one that I could find that works. Pass the _oleobj_ 
+            # of the IDispatch to the IModelCenter constructor. We will need to use this pattern
+            # all over the place.
             workflow_instance = mclib.IModelCenter(self._instance.parallelInstance()._oleobj_)
             workflow_instance.loadModel(file_name, mclib.constants.CONN_ERR_ERROR)
             return _WorkflowImpl(workflow_instance, self._to_thread)
