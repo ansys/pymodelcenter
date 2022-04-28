592a865f1c4bd02c4aa5922ceb1c6bd42ccba26c
phx-mpascale <mpascale@phoenix-int.com>
2022-04-28 16:56:31 -0400

    Implementations of Workflow: run, get_data_monitor, create_data_monitor, remove_data_monitor, get_data_explorer.

--- ansys/modelcenter/workflow/api/__init__.py
+++ ansys/modelcenter/workflow/api/__init__.py
@@ -1,5 +1,6 @@
 from .data_explorer import DataExplorer
 from .engine import Engine, EngineInfo, OnConnectionErrorMode, WorkflowType
 from .icomponent import IComponent
+from .idatamonitor import IDataMonitor
 from .iformat import IFormat
 from .workflow import Workflow
