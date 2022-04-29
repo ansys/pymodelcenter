6110b7bb1adef060baf1b6c807fe05ba6a2df1c2
Mark Koninckx <mark.koninckx@ansys.com>
2022-04-29 13:18:32 -0400

    Remove IAssemblies, since we can represent it better with standard Python duck types.

--- ansys/modelcenter/workflow/api/__init__.py
+++ ansys/modelcenter/workflow/api/__init__.py
@@ -1,6 +1,5 @@
 from .data_explorer import DataExplorer
 from .engine import Engine, EngineInfo, OnConnectionErrorMode, WorkflowType
-from .iassemblies import IAssemblies
 from .iassembly import IAssembly
 from .icomponent import IComponent
 from .idatamonitor import IDataMonitor
