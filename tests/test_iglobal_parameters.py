5539dc786860a97112784de571df3ddd4ac22d2b
Mark Potter <mpotter@phoenix-int.com>
2022-04-29 12:26:06 -0400

    Minor correction to test stubs.

--- tests/test_iglobal_parameters.py
+++ tests/test_iglobal_parameters.py
@@ -2,30 +2,30 @@ import pytest
 
 
 @pytest.mark.skip(reason="Not implemented.")
-def test_count(self) -> None:
+def test_count() -> None:
     """Testing of the count property."""
     raise NotImplementedError
 
 
 @pytest.mark.skip(reason="Not implemented.")
-def test_get_item(self) -> None:
+def test_get_item() -> None:
     """Testing of the get_item method."""
     raise NotImplementedError
 
 
 @pytest.mark.skip(reason="Not implemented.")
-def test_set_item(self) -> None:
+def test_set_item() -> None:
     """Testing of the set_item method."""
     raise NotImplementedError
 
 
 @pytest.mark.skip(reason="Not implemented.")
-def test_set_sxport_to_remote_components(self) -> None:
+def test_set_sxport_to_remote_components() -> None:
     """Testing of the set_sxport_to_remote_components method."""
     raise NotImplementedError
 
 
 @pytest.mark.skip(reason="Not implemented.")
-def test_remove(self) -> None:
+def test_remove() -> None:
     """Testing of the remove method."""
     raise NotImplementedError
