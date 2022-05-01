8600c745c3bd53324a6fb452dd2875e4d9605ba6
Mark Potter <mpotter@phoenix-int.com>
2022-05-01 01:19:34 -0400

    Added implementation of base class usable for IGroups and other arrayish API interfaces.

--- ansys/modelcenter/workflow/api/igroups.py
+++ ansys/modelcenter/workflow/api/igroups.py
@@ -1,24 +1,17 @@
-class IGroups:
+from .arrayish import Arrayish
+from .igroup import IGroup
+
+
+class IGroups(Arrayish[IGroup]):
     """COM Instance."""
 
-    def count(self):
-        """Number of Groups."""
-        # VARIANT Count;
-        raise NotImplementedError
-
-    def item(self, id_) -> object:
+    def __init__(self, instance) -> None:
         """
-        Gets a pointer to the specified Group.
+        Initialize an arrayish collection of IGroup objects.
 
         Parameters
         ----------
-        id_ :
-            ID of the specified Group. It can be a name or an index
-            (0-based index).
-
-        Returns
-        -------
-        An IGroup object.
+        instance :
+            ModelCenter API IGroups interface object.
         """
-        # VARIANT Item(VARIANT id);
-        raise NotImplementedError
+        Arrayish.__init__(self, instance, IGroup)
