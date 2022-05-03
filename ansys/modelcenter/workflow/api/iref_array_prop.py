7262de36eb3e7d8e89d0a13cb0727ddc77e87b8e
bfairburn-phx <brandon.fairburn@ansys.com>
2022-05-03 16:58:42 -0400

    IRefArrayProp implementation

--- ansys/modelcenter/workflow/api/iref_array_prop.py
+++ ansys/modelcenter/workflow/api/iref_array_prop.py
@@ -1,28 +1,54 @@
+import clr
+clr.AddReference('phoenix-mocks/Phoenix.Mock.v45')
+from Phoenix.Mock import MockRefArrayProp
+
+
 class IRefArrayProp:
     """COM Instance."""
+
+    def __init__(self, name: str, type_: str):
+        self._instance = MockRefArrayProp(name, type_)
 
     @property
     def enum_values(self) -> str:
         """Enumerated values of the reference array property."""
-        # BSTR enumValues;
-        raise NotImplementedError
+        return self._instance.enumValues
+
+    @enum_values.setter
+    def enum_values(self, value: str) -> None:
+        """Enumerated values of the reference array property."""
+        self._instance.enumValues = value
 
     @property
     def is_input(self) -> bool:
-        """Enumerated values of the reference array property."""
+        """Gets a flag that is true if this property is an input."""
         # boolean isInput;
-        raise NotImplementedError
+        return self._instance.isInput
+
+    @is_input.setter
+    def is_input(self, value):
+        """Gets a flag that is true if this property is an input."""
+        self._instance.isInput = value
 
     @property
     def title(self) -> str:
         """Title of the reference array property."""
-        # BSTR title;
-        raise NotImplementedError
+        return self._instance.title
+
+    @title.setter
+    def title(self, title_: str) -> None:
+        """Title of the reference array property."""
+        self._instance.title = title_
 
     @property
     def description(self) -> str:
         """Description of the reference array property."""
-        # BSTR description;
+        return self._instance.description
+
+    @description.setter
+    def description(self, value):
+        """Description of the reference array property."""
+        self._instance.description = value
 
     def get_name(self) -> str:
         """
@@ -30,10 +56,10 @@ class IRefArrayProp:
 
         Returns
         -------
-        The name of the reference array property.
+        str
+            The name of the reference array property.
         """
-        # BSTR getName();
-        raise NotImplementedError
+        return self._instance.getName()
 
     def get_type(self) -> str:
         """
@@ -41,7 +67,7 @@ class IRefArrayProp:
 
         Returns
         -------
-        The type of the reference array property.
+        str
+            The type of the reference array property.
         """
-        # BSTR getType();
-        raise NotImplementedError
+        return self._instance.getType()
