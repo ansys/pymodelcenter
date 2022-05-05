afbb3f1b2a8ff59aaa114915628cf627e16061f8
Mark Potter <mpotter@phoenix-int.com>
2022-05-05 16:09:44 -0400

    Automated style corrections.

--- ansys/modelcenter/workflow/api/icomponent.py
+++ ansys/modelcenter/workflow/api/icomponent.py
@@ -6,8 +6,7 @@ from System import String as DotNetString
 import clr
 
 from ansys.modelcenter.workflow.api.assembly import Assembly
-from ansys.modelcenter.workflow.api.dot_net_utils import DotNetListConverter
-from ansys.modelcenter.workflow.api.dot_net_utils import IVariableConverter
+from ansys.modelcenter.workflow.api.dot_net_utils import DotNetListConverter, IVariableConverter
 from ansys.modelcenter.workflow.api.igroups import IGroups
 from ansys.modelcenter.workflow.api.ivariables import IVariables
 
