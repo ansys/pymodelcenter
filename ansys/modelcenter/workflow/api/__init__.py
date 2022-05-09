548657559526ce918dd828b4c5082ff79405f56a
Mark Potter <mpotter@phoenix-int.com>
2022-05-09 09:29:15 -0400

    Changes to get building after merge.

--- ansys/modelcenter/workflow/api/__init__.py
+++ ansys/modelcenter/workflow/api/__init__.py
@@ -10,8 +10,8 @@ from .iglobal_parameters import IGlobalParameters
 from .igroup import IGroup
 from .irefprop import IRefArrayProp, IRefProp
 from .iif_component import IIfComponent
-from .iref_array_prop import IRefArrayProp
-from .iref_prop import IRefProp
+# from .iref_array_prop import IRefArrayProp
+from .irefprop import IRefProp
 from .ireference_array import IReferenceArray
 from .ireference_variable import IReferenceVariable
 from .iscript_component import IScriptComponent
