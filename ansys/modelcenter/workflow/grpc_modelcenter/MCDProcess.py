717551b4093420f0c2da9fce115be2c13b87e6aa
bfairburn-phx <brandon.fairburn@ansys.com>
2023-02-16 10:03:30 -0500

    Separate process startup from constructor.

--- ansys/modelcenter/workflow/grpc_modelcenter/MCDProcess.py
+++ ansys/modelcenter/workflow/grpc_modelcenter/MCDProcess.py
@@ -2,6 +2,17 @@ import subprocess
 import winreg
 
 
+def _find_exe_location() -> str:
+    """Attempts to find ModelCenter.exe"""
+
+    key: winreg.HKEYType = winreg.OpenKey(
+        winreg.HKEY_LOCAL_MACHINE,
+        r"SOFTWARE\Phoenix Integration\ModelCenter")
+
+    install_dir: str = winreg.QueryValueEx(key, "CurrentInstallLocation")[0]
+    return f"{install_dir}\\ModelCenter.exe"
+
+
 class MCDProcess:
     """
     Responsible for launching and keeping track of a ModelCenter Desktop
@@ -12,13 +23,12 @@ class MCDProcess:
     """
 
     def __init__(self):
-        self._exe_path: str = self._find_exe_location()
+        self._exe_path: str = _find_exe_location()
+        self._process = None
+
+    def start(self):
+        """Start the MCD process."""
+
+        # May want to use some additional parameters for capturing stdout, etc.
         self._process = subprocess.Popen(self._exe_path)
 
-    def _find_exe_location(self) -> str:
-        key: winreg.HKEYType = winreg.OpenKey(
-            winreg.HKEY_LOCAL_MACHINE,
-            r"SOFTWARE\Phoenix Integration\ModelCenter")
-
-        install_dir: str =  winreg.QueryValueEx(key, "CurrentInstallLocation")[0]
-        return f"{install_dir}\\ModelCenter.exe"
