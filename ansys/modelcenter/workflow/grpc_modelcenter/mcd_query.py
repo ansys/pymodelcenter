1b1d01a895c1433e9ecb0008f5b6770f289db063
bfairburn-phx <brandon.fairburn@ansys.com>
2023-02-20 08:07:15 -0500

    Beginning of GRPC query module.

---
+++ ansys/modelcenter/workflow/grpc_modelcenter/mcd_query.py
@@ -0,0 +1,28 @@
+"""
+Module containing methods to query the MCD GRPC server.
+"""
+
+
+import grpc
+from ansys.modelcenter.workflow.grpc_modelcenter.proto.modelcenter_pb2_grpc import *
+from ansys.modelcenter.workflow.grpc_modelcenter.proto.modelcenter_pb2 import *
+
+
+def get_engine_info(target: str = "localhost:50051") -> EngineInfoResponse:
+    """
+    GRPC query to GetEngineInfo().
+
+    Parameters
+    ----------
+    target : str
+        Target for the query; string of the form HOST_ADDRESS:PORT
+
+    Returns
+    -------
+    EngineInfoResponse
+        Result of the query.
+    """
+    channel = grpc.insecure_channel(target)
+    stub = EngineStub(channel)
+    return stub.GetEngineInfo()
+    
--- protos/generate-proto.bat
+++ protos/generate-proto.bat
@@ -1,3 +1,3 @@
 mkdir ansys\modelcenter\workflow\grpc_modelcenter\proto
-python -m grpc_tools.protoc -I./protos --python_out=./ansys/modelcenter/workflow/grpc_modelcenter/proto --grpc_python_out=./ansys/modelcenter/workflow/grpc_modelcenter/proto ./protos/ModelCenter-standin.proto
+python -m grpc_tools.protoc -I./protos --python_out=./ansys/modelcenter/workflow/grpc_modelcenter/proto --grpc_python_out=./ansys/modelcenter/workflow/grpc_modelcenter/proto ./protos/modelcenter.proto
 
