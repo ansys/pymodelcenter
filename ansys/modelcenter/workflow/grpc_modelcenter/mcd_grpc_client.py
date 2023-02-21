e895b816eaaa9fc461e0b835acff401022214b37
bfairburn-phx <brandon.fairburn@ansys.com>
2023-02-21 06:06:34 -0500

    Add Shutdown(), LoadWorkflow(), NewWorkflow()

--- ansys/modelcenter/workflow/grpc_modelcenter/mcd_grpc_client.py
+++ ansys/modelcenter/workflow/grpc_modelcenter/mcd_grpc_client.py
@@ -4,24 +4,92 @@ Module containing methods to query the MCD GRPC server.
 
 
 import grpc
-from ansys.modelcenter.workflow.grpc_modelcenter.proto.modelcenter_pb2_grpc import *  # type: ignore
-from ansys.modelcenter.workflow.grpc_modelcenter.proto.modelcenter_pb2 import *  # type: ignore
+from enum import Enum
+import ansys.modelcenter.workflow.grpc_modelcenter.proto.modelcenter_pb2_grpc as mcd_grpc
+import ansys.modelcenter.workflow.grpc_modelcenter.proto.modelcenter_pb2 as mcd
 
 
-def get_engine_info(target: str = "localhost:50051") -> EngineInfoResponse:
+class WorkflowType(Enum):
+    DATA = 0
+    PROCESS = 1
+
+
+def get_engine_info(target: str = "localhost:50051") -> mcd.EngineInfoResponse:
     """
     GRPC query to GetEngineInfo().
 
     Parameters
     ----------
     target : str
-        Target for the query; string of the form HOST_ADDRESS:PORT
+        Target for the query; string of the form HOST_ADDRESS:PORT.
+        Defaults to "localhost:50051".
 
     Returns
     -------
     EngineInfoResponse
         Result of the query.
     """
-    channel = grpc.insecure_channel(target)
-    stub = EngineStub(channel)
-    return stub.GetEngineInfo()
+    with grpc.insecure_channel(target) as channel:
+        stub = mcd_grpc.EngineStub(channel)
+        return stub.GetEngineInfo()
+
+
+def shutdown(target: str = "localhost:50051") -> None:
+    """
+    GRPC query to Shutdown().
+
+    Parameters
+    ----------
+    target : str
+        Target for the query; string of the form HOST_ADDRESS:PORT.
+        Defaults to "localhost:50051".
+    """
+    with grpc.insecure_channel(target) as channel:
+        stub = mcd_grpc.EngineStub(channel)
+        stub.Shutdown()
+
+
+def load_workflow(path: str, target: str = "localhost:50051") -> None:
+    """
+    GRPC query to LoadWorkflow().
+
+    Parameters
+    ----------
+    path : str
+        Path to the workflow to be opened.
+
+    target : str
+        Target for the query; string of the form HOST_ADDRESS:PORT.
+        Defaults to "localhost:50051".
+    """
+    request = mcd.LoadWorkflowRequest()
+    request.path = path
+
+    with grpc.insecure_channel(target) as channel:
+        stub = mcd_grpc.EngineStub(channel)
+        stub.LoadWorkflow(request)
+
+
+def new_workflow(name: str, type_: WorkflowType, target: str = "localhost:50051") -> None:
+    """
+    GRPC query to NewWorkflow().
+
+    Parameters
+    ----------
+    name : str
+        Name of new workflow to create.
+
+    type_ : WorkflowType
+        Type of workflow to create.
+
+    target : str
+        Target for the query; string of the form HOST_ADDRESS:PORT.
+        Defaults to "localhost:50051".
+    """
+    request = mcd.NewWorkflowRequest()
+    request.name = name
+    request.type = type_
+
+    with grpc.insecure_channel(target) as channel:
+        stub = mcd_grpc.EngineStub(channel)
+        stub.NewWorkflow(request)
