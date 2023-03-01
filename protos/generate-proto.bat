6fd980fa448036fcc509a4eb636df2dea07fd22b
bfairburn-phx <brandon.fairburn@ansys.com>
2023-03-01 14:49:53 -0500

    Include __init__.py in generated GRPC code to allow relative imports internally.

--- protos/generate-proto.bat
+++ protos/generate-proto.bat
@@ -1,3 +1,2 @@
-mkdir ansys\modelcenter\workflow\grpc_modelcenter\proto
 python -m grpc_tools.protoc -I./protos --python_out=./ansys/modelcenter/workflow/grpc_modelcenter/proto --grpc_python_out=./ansys/modelcenter/workflow/grpc_modelcenter/proto ./protos/modelcenter.proto
 
