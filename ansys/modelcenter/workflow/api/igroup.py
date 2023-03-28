2b5de9b8f19ad4d959f839d698ec0613c1f91ee1
Mark Koninckx <mkoninckx@phoenix-int.com>
2023-03-28 16:45:37 -0400

    Remove .NET implementation from IGroup & streamline.

--- ansys/modelcenter/workflow/api/igroup.py
+++ ansys/modelcenter/workflow/api/igroup.py
@@ -1,50 +1,31 @@
 """Definition of group of variables."""
 from __future__ import annotations
 
-from typing import Collection, Sequence
+from abc import ABC, abstractmethod
+from typing import Collection
 
 from ansys.engineeringworkflow.api import IVariableContainer
-import clr
-from overrides import overrides
-
-from ansys.modelcenter.workflow.api.arrayish import Arrayish
-import ansys.modelcenter.workflow.api.dot_net_utils as utils
-import ansys.modelcenter.workflow.api.ivariable as ivariable
-
-clr.AddReference("phoenix-mocks/Interop.ModelCenter")
-from ModelCenter import IGroup as mcapiIGroup  # type: ignore
 
 
-class IGroup(IVariableContainer):
-    """COM Instance."""
-
-    @overrides
-    def get_variables(self) -> Collection[ivariable.IVariable]:
-        return Arrayish(self._instance.Variables, utils.from_dot_net_to_ivariable)
-
-    def __init__(self, group: mcapiIGroup):
-        """Initialize."""
-        self._instance = group
+class IGroup(IVariableContainer, ABC):
+    """Represents a variable group in ModelCenter."""
 
     @property
-    def groups(self) -> "Sequence[IGroup]":
-        """The Groups this Group is a member of."""
-        return Arrayish(self._instance.Groups, IGroup)
+    @abstractmethod
+    def groups(self) -> Collection["IGroup"]:
+        """The groups that this group contains."""
+        raise NotImplementedError()
 
+    # TODO / REDUCE: Consider dropping for Phase II or entirely
     @property
+    @abstractmethod
     def icon_id(self) -> int:
         """The ID number of the icon to use for the Group."""
-        return self._instance.iconId
+        raise NotImplementedError()
 
+    # TODO / REDUCE: Consider dropping for Phase II or entirely
     @icon_id.setter
+    @abstractmethod
     def icon_id(self, id: int) -> None:
         """The ID number of the icon to use for the Group."""
-        self._instance.iconId = id
-
-    def get_name(self) -> str:
-        """Gets the name of the Group."""
-        return self._instance.getName()
-
-    def get_full_name(self) -> str:
-        """Gets the full %ModelCenter path of the Group."""
-        return self._instance.getFullName()
+        raise NotImplementedError()
