a63a8a18927e097cc35c0253f889391b62445e36
Mark Koninckx <mkoninckx@phoenix-int.com>
2023-03-30 09:20:06 -0400

    Expand abstract_workflow_element to inherit / implement IElement.

--- ansys/modelcenter/workflow/grpc_modelcenter/abstract_workflow_element.py
+++ ansys/modelcenter/workflow/grpc_modelcenter/abstract_workflow_element.py
@@ -1,14 +1,20 @@
 """Defines an abstract base class for gRPC-backed workflow elements."""
 
 from abc import ABC
+from typing import Collection
 
+import ansys.common.variableinterop as acvi
+import ansys.engineeringworkflow.api as aew_api
 import grpc
+from overrides import overrides
 
+from .proto.custom_metadata_messages_pb2 import MetadataGetValueRequest, MetadataSetValueRequest
 from .proto.element_messages_pb2 import ElementId
 from .proto.grpc_modelcenter_workflow_pb2_grpc import ModelCenterWorkflowServiceStub
+from .var_value_convert import convert_grpc_value_to_acvi, convert_interop_value_to_grpc
 
 
-class AbstractWorkflowElement(ABC):
+class AbstractWorkflowElement(aew_api.IElement, ABC):
     """An abstract base class for gRPC-backed workflow elements."""
 
     def _create_client(self, channel: grpc.Channel) -> ModelCenterWorkflowServiceStub:
@@ -27,10 +33,46 @@ class AbstractWorkflowElement(ABC):
         self._client: ModelCenterWorkflowServiceStub = self._create_client(channel)
         self._element_id: ElementId = element_id
 
-    def get_name(self) -> str:
-        """Get the short name of the element (relative to its parent)."""
-        return self._client.ElementGetName(self._element_id).name
+    @property
+    @overrides
+    def element_id(self) -> str:
+        return self._element_id.id_string
 
-    def get_full_name(self) -> str:
-        """Get the full name of the element (relative to its parent)."""
-        return self._client.ElementGetFullName(self._element_id).name
+    @property
+    @overrides
+    def parent_element_id(self) -> str:
+        result = self._client.ElementGetParentElement(self._element_id)
+        return result.id_string
+
+    @property
+    @overrides
+    def name(self) -> str:
+        result = self._client.ElementGetFullName(self._element_id)
+        return result.name
+
+    @overrides
+    def get_property(self, property_name: str) -> aew_api.Property:
+        grpc_value = self._client.PropertyOwnerGetPropertyValue(
+            MetadataGetValueRequest(id=self._element_id, property_name=property_name)
+        )
+        acvi_value = convert_grpc_value_to_acvi(grpc_value)
+        return aew_api.Property(
+            parent_element_id=self._element_id.id_string,
+            property_name=property_name,
+            property_value=acvi_value,
+        )
+
+    @overrides
+    def get_properties(self) -> Collection[aew_api.Property]:
+        names = self._client.PropertyOwnerGetProperties()
+        name: str
+        return [self.get_property(property_name=name) for name in names]
+
+    @overrides
+    def set_property(self, property_name: str, property_value: acvi.IVariableValue) -> None:
+        grpc_value = convert_interop_value_to_grpc(property_value)
+        self._client.PropertyOwnerSetPropertyValue(
+            MetadataSetValueRequest(
+                id=self._element_id, property_name=property_name, value=grpc_value
+            )
+        )
