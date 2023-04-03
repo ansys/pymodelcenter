49ec9bb16d092c4c9a3e5ce6c62b27782ad05ef2
phx-mpascale <mpascale@phoenix-int.com>
2023-04-03 14:54:36 -0400

    Added variable wrappers to __init__.py.

--- ansys/modelcenter/workflow/grpc_modelcenter/__init__.py
+++ ansys/modelcenter/workflow/grpc_modelcenter/__init__.py
@@ -5,9 +5,13 @@ This implementation will find a local installation of ModelCenter, launch it,
 and attempt to communicate with it via gRPC.
 """
 
+from .boolean_variable import BooleanVariable, BooleanArray
 from .component import Component
 from .engine import Engine
+from .double_variable import DoubleVariable, DoubleArray
 from .format import Format
+from .integer_variable import IntegerVariable, IntegerArray
 from .mcd_process import MCDProcess
+from .string_variable import StringVariable, StringArray
 from .variable_link import VariableLink
 from .workflow import Workflow
