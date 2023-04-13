ddf133295cfd3bc644e8c770ebad3d2f14e618d6
Mark Koninckx <mkoninckx@phoenix-int.com>
2023-04-13 14:19:36 -0400

    Expose exception types on grpcmc __init__.py

--- ansys/modelcenter/workflow/grpc_modelcenter/__init__.py
+++ ansys/modelcenter/workflow/grpc_modelcenter/__init__.py
@@ -7,12 +7,25 @@ and attempt to communicate with it via gRPC.
 
 from .assembly import Assembly
 from .boolean_variable import BooleanArrayVariable, BooleanVariable
-from .component import Component
+from .component import (
+    Component,
+    ComponentDownloadValuesFailedError,
+    ComponentReconnectionFailedError,
+)
 from .double_variable import RealArrayVariable, RealVariable
 from .engine import Engine
 from .format import Format
+from .group import Group
+from .grpc_error_interpretation import (
+    EngineDisconnectedError,
+    EngineInternalError,
+    InvalidInstanceError,
+    UnexpectedEngineError,
+)
 from .integer_variable import IntegerArray, IntegerVariable
 from .mcd_process import MCDProcess
 from .string_variable import StringArrayVariable, StringVariable
+from .unsupported_var import UnsupportedTypeVariable, VariableWithUnsupportedTypeException
+from .var_value_convert import ValueTypeNotSupportedError
 from .variable_link import VariableLink
 from .workflow import Workflow
