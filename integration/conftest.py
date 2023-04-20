3bce24679906b90291f11f6602c9773433ba27d5
phx-mpascale <mpascale@ansys.com>
2023-04-20 11:15:58 -0400

    Expanded load_workflow to allow for specifying a different file to load via test mark.

--- integration/conftest.py
+++ integration/conftest.py
@@ -17,7 +17,13 @@ def create_engine() -> Generator[grpcmc.Engine, None, None]:
 
 
 @pytest.fixture(name="workflow")
-def load_workflow(engine) -> Generator[grpcmc.Workflow, None, None]:
-    workflow_path: str = os.path.join(os.getcwd(), "test_files", "all_types.pxcz")
+def load_workflow(engine, request) -> Generator[grpcmc.Workflow, None, None]:
+    file_name: str
+    marker = request.node.get_closest_marker("workflow_name")
+    if marker is None:
+        file_name = "all_types.pxcz"
+    else:
+        file_name = marker.args[0]
+    workflow_path: str = os.path.join(os.getcwd(), "test_files", file_name)
     with engine.load_workflow(file_name=workflow_path) as workflow:
         yield workflow
