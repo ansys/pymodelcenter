f0c1a0bc21064939acbae53d17822e3431c8e19c
phx-mpascale <mpascale@ansys.com>
2023-04-25 09:28:55 -0400

    Add project root to path and attempt to manually import packages to see if we get a better error message.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,12 @@
 """Sphinx documentation configuration file."""
 from datetime import datetime
 import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# import now to check for errors
+import ansys.modelcenter.workflow.api
+import ansys.modelcenter.workflow.grpc_modelcenter
 
 from ansys_sphinx_theme import get_version_match
 from ansys_sphinx_theme import pyansys_logo_black as logo
