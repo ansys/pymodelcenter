7c9fa32acc038a19c40dc29eae8c0e03302851bb
Mark Koninckx <mkoninckx@phoenix-int.com>
2023-05-01 11:02:56 -0400

    Remove resolved TODO about missing file_value case.

--- ansys/modelcenter/workflow/grpc_modelcenter/var_value_convert.py
+++ ansys/modelcenter/workflow/grpc_modelcenter/var_value_convert.py
@@ -141,7 +141,6 @@ def convert_grpc_value_to_acvi(original: VariableValue) -> acvi.IVariableValue:
             original.string_array_value.dims.dims,
             np.reshape(original.string_array_value.values, original.string_array_value.dims.dims),
         )
-    # TODO: FileValue missing from gRPC API, need to add, update this case
     elif original.HasField("file_value"):
         raise ValueTypeNotSupportedError(
             "The provided gRPC value has a file type, "
