8652db5de617b216051a3094b672a535da2b3a6c
Mark Koninckx <mark.koninckx@ansys.com>
2023-05-18 11:40:23 -0400

    Implement and test link suspend / resume. (#129)

--- integration/test_links.py
+++ integration/test_links.py
@@ -2,6 +2,7 @@
 from typing import List
 import unittest
 
+import ansys.tools.variableinterop as atvi
 import pytest
 
 import ansys.modelcenter.workflow.api as mcapi
@@ -36,3 +37,56 @@ def test_can_break_a_link(workflow) -> None:
     links = list(workflow.get_links())
     assert len(links) == 3
     assert link not in links
+
+
+@pytest.mark.workflow_name("linked_quadratics.pxcz")
+def test_can_suspend_and_resume_a_link(workflow) -> None:
+    # Setup: Get all the links in the model as well as the link we think is involved.
+    links = {link.lhs: link for link in workflow.get_links()}
+    upstream_var: mcapi.IDatapin = workflow.get_variable("Model.Quadratic.x")
+    downstream_var: mcapi.IDatapin = workflow.get_variable("Model.Quadratic1.x")
+    control_upstream_var: mcapi.IDatapin = workflow.get_variable("Model.Quadratic.a")
+    control_downstream_var: mcapi.IDatapin = workflow.get_variable("Model.Quadratic1.a")
+
+    # Setup: Get the link we intend to suspend. Verify it looks like we think.
+    assert downstream_var.element_id in links
+    link = links[downstream_var.element_id]
+    assert link.rhs == upstream_var.full_name
+
+    # Setup: Get another link we don't intend to suspend. Verify it looks like we think.
+    assert control_downstream_var.element_id in links
+    control_link = links[control_downstream_var.element_id]
+    assert control_link.rhs == control_upstream_var.full_name
+
+    # Execute: Suspend the target link
+    link.suspend()
+
+    # Verify: Set the upstream side of the control and suspended links.
+    upstream_var.set_value(atvi.VariableState(atvi.RealValue(4.7), True))
+    control_upstream_var.set_value(atvi.VariableState(atvi.RealValue(867.5309), True))
+
+    # Verify: Check that the downstream side has the value we expect.
+    assert downstream_var.get_value().value == atvi.RealValue(1.0)
+    assert control_downstream_var.get_value().value == atvi.RealValue(1.0)
+
+    # Verify: Run the workflow.
+    workflow.run()
+
+    # Verify: The downstream side should not have been changed for the suspended link,
+    assert downstream_var.get_value().safe_value == atvi.RealValue(1.0)
+    # but it should have been for the control link.
+    assert control_downstream_var.get_value().safe_value == atvi.RealValue(867.5309)
+
+    # Execute, stage II: Resume the target link
+    link.resume()
+
+    # Verify: Set the upstream side of the control and resumed links.
+    upstream_var.set_value(atvi.VariableState(atvi.RealValue(17.76), True))
+    control_upstream_var.set_value(atvi.VariableState(atvi.RealValue(9000.1), True))
+
+    # Verify: Run the workflow.
+    workflow.run()
+
+    # Verify: The downstream side should be set for both links.
+    assert downstream_var.get_value().safe_value == atvi.RealValue(17.76)
+    assert control_downstream_var.get_value().safe_value == atvi.RealValue(9000.1)
--- tests/test_variable_link.py
+++ tests/test_variable_link.py
@@ -19,6 +19,11 @@ class MockWorkflowClientForLinkTest(ModelCenterWorkflowServiceStub):
     ) -> workflow_msg.WorkflowBreakLinkResponse:
         return workflow_msg.WorkflowBreakLinkResponse()
 
+    def WorkflowSuspendOrResumeLink(
+        self, request: workflow_msg.WorkflowSuspendOrResumeLinkRequest
+    ) -> workflow_msg.WorkflowLinkSuspension:
+        return workflow_msg.WorkflowLinkSuspension()
+
 
 def test_break_existing_link(monkeypatch) -> None:
     # Setup
@@ -59,5 +64,43 @@ def test_break_missing_link(monkeypatch) -> None:
         # Verification
         expected_request = workflow_msg.WorkflowBreakLinkRequest(
             target_var=ElementId(id_string=mock_lhs)
+        )
+        mock_grpc_method.assert_called_once_with(expected_request)
+
+
+def test_suspend_link(monkeypatch) -> None:
+    # Setup
+    mock_client = MockWorkflowClientForLinkTest()
+    mock_response = workflow_msg.WorkflowLinkSuspension()
+    mock_lhs = "TEST_LHS"
+    mock_rhs = "TEST_RHS"
+    with unittest.mock.patch.object(
+        mock_client, "WorkflowSuspendOrResumeLink", return_value=mock_response
+    ) as mock_grpc_method:
+        sut = DatapinLink(mock_client, mock_lhs, mock_rhs)
+
+        sut.suspend()
+
+        expected_request = workflow_msg.WorkflowSuspendOrResumeLinkRequest(
+            target_link_lhs=ElementId(id_string=mock_lhs), suspend=True
+        )
+        mock_grpc_method.assert_called_once_with(expected_request)
+
+
+def test_resume_link(monkeypatch) -> None:
+    # Setup
+    mock_client = MockWorkflowClientForLinkTest()
+    mock_response = workflow_msg.WorkflowLinkSuspension()
+    mock_lhs = "TEST_LHS"
+    mock_rhs = "TEST_RHS"
+    with unittest.mock.patch.object(
+        mock_client, "WorkflowSuspendOrResumeLink", return_value=mock_response
+    ) as mock_grpc_method:
+        sut = DatapinLink(mock_client, mock_lhs, mock_rhs)
+
+        sut.resume()
+
+        expected_request = workflow_msg.WorkflowSuspendOrResumeLinkRequest(
+            target_link_lhs=ElementId(id_string=mock_lhs), suspend=False
         )
         mock_grpc_method.assert_called_once_with(expected_request)
