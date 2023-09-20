28d8d1dd57c1bacad22cf5cbbae4f61890deadbf
Mark Koninckx <mkoninckx@phoenix-int.com>
2023-09-20 17:19:40 -0400

    Create DriverComponent implementation and use it.

---
+++ ansys/modelcenter/workflow/grpc_modelcenter/driver_component.py
@@ -0,0 +1,39 @@
+"""Contains a gRPC-backed implementation of IDriverComponent."""
+from typing import TYPE_CHECKING
+
+from .abstract_control_statement import AbstractControlStatement
+from .component import Component
+
+if TYPE_CHECKING:
+    from .engine import Engine
+
+from ansys.api.modelcenter.v0.element_messages_pb2 import ElementId
+
+import ansys.modelcenter.workflow.api as mc_api
+
+
+class DriverComponent(
+    Component,
+    AbstractControlStatement,
+    mc_api.IDriverComponent,
+):
+    """
+    A driver component in a workflow.
+
+    In process-mode workflows, driver components can contain children.
+    In data-mode workflows, driver components should still be instantiated with an instance
+    of this class, but the method to get child elements will simply return an empty collection.
+    """
+
+    def __init__(self, element_id: ElementId, engine: "Engine"):
+        """
+        Initialize a new instance.
+
+        Parameters
+        ----------
+        element_id : ElementId
+            The id of the component.
+        engine : Engine
+            The Engine that created this component.
+        """
+        super(DriverComponent, self).__init__(element_id=element_id, engine=engine)
