878a22c8dc0579aa5c4f99c3cd32888587c60647
Joshua Edwards <109987864+ejdubu11@users.noreply.github.com>
2026-01-16 13:58:15 -0500

    Corrected typo for pre-commit failure

--- examples/TradeStudy_Optimization.py
+++ examples/TradeStudy_Optimization.py
@@ -25,28 +25,27 @@
 # nested under it. It then configures and runs the optimization
 
 import os
-
-from ansys.tools.variableinterop.scalar_values import RealValue, StringValue
+from ansys.tools.variableinterop.scalar_values import RealValue
+from ansys.tools.variableinterop.scalar_values import StringValue
 from ansys.tools.variableinterop.variable_state import VariableState
-
-import ansys.modelcenter.workflow.api as mcapi
 import ansys.modelcenter.workflow.grpc_modelcenter as grpcmc
+import ansys.modelcenter.workflow.api as mcapi
 
 # create workflow relative to script
 cwd = os.getcwd()
 workflow_path = os.path.join(cwd, "Optimization.pxcz")
 
-# contant value to assign to a variable not used in optimization design variables
+# constant value to assign to a variable not used in optimization design variables
 newConstantVal = 3
 
-with grpcmc.Engine() as mc:
+with grpcmc.Engine() as mc:    
     with mc.new_workflow(workflow_path, mcapi.WorkflowType.PROCESS) as workflow:
         print("\n- workflow created: " + workflow_path)
-
+        
         # get workflow root element
         workflowRoot = workflow.get_root()
-
-        # create Opt Tool component
+        
+        # create Opt Tool component        
         optComp = workflow.create_component(
             "component plug-in:SOFTWARE\\Phoenix Integration\\"
             "Component Plug-Ins\\Optimization Tool",
@@ -55,12 +54,14 @@ with grpcmc.Engine() as mc:
         )
         print("- Optimization component created")
 
-        # create Quadratic component under Opt Tool component
+        # create Quadratic component under Opt Tool component 
         targetComp = workflow.create_component(
-            "common:\\Functions\\Quadratic", "Quadratic", optComp
+            "common:\\Functions\\Quadratic", 
+            "Quadratic", 
+            optComp
         )
         print("- Quadratic component created under Optimization component")
-
+        
         # get existing hardcoded datapin values and states
         inVarName = targetComp.full_name + ".x"
         extraInVarName = targetComp.full_name + ".c"
@@ -68,73 +69,62 @@ with grpcmc.Engine() as mc:
         inDatapin = workflow.get_datapin(inVarName)
         extraInDatapin = workflow.get_datapin(extraInVarName)
         outDatapin = workflow.get_datapin(outVarName)
-        print(
-            f"- existing value for {inVarName}: {inDatapin.get_state().value} "
-            f"(valid={inDatapin.get_state().is_valid})"
-        )
-        print(
-            f"- existing value for {outVarName}: {outDatapin.get_state().value} "
-            f"(valid={outDatapin.get_state().is_valid})"
-        )
+        print(f'- existing value for {inVarName}: {inDatapin.get_state().value} ' 
+            f'(valid={inDatapin.get_state().is_valid})')
+        print(f'- existing value for {outVarName}: {outDatapin.get_state().value} ' 
+            f'(valid={outDatapin.get_state().is_valid})')
 
-        # set additional input variable that isn't an optimization design variable
+        # set additional input variable that isn't an optimization design variable 
         # to new value
         extraInDatapin.set_state(VariableState(RealValue(newConstantVal), True))
-        print(
-            f"- new value for {extraInVarName} set to: {extraInDatapin.get_state().value} "
-            f"(valid={extraInDatapin.get_state().is_valid})"
-        )
-
+        print(f'- new value for {extraInVarName} set to: {extraInDatapin.get_state().value} '
+            f'(valid={extraInDatapin.get_state().is_valid})')
+        
         # set Opt Tool component datapins
         algorithmDatapin = optComp.get_datapins()["algorithm"]
         algorithmDatapin.set_state(VariableState("com.ansys.osl_algorithm.NLPQLP", True))
         print(f'\n- algorithm set to "Optislang NLPQLP algorithm"')
-
-        objectiveDatapin = optComp.get_datapins()["objectives"]
+        
+        objectiveDatapin = optComp.get_datapins()['objectives'] 
         objectiveDatapin.set_length(1)
-        objectiveDatapin[0].equation = outVarName
+        objectiveDatapin[0].equation = outVarName  
         print(f'- objective set to "{objectiveDatapin[0].equation}"')
-
+        
         objective_refProps = objectiveDatapin.get_reference_properties()
         objective_refProps["goal"].set_value_at(0, VariableState("solveFor", True))
         print(f'- objective goal set to "solveFor: 0"')
-
+        
         contDesignVars = optComp.get_datapins()["continuousDesignVariables"]
         contDesignVars.set_length(1)
-        contDesignVars[0].equation = inVarName
+        contDesignVars[0].equation = inVarName 
         print(f'- continuous design variable set to "{contDesignVars[0].equation}"')
-
+        
         contDesignVars_refProps = contDesignVars.get_reference_properties()
         contDesignVars_refProps["startValue"].set_value_at(
-            0, VariableState(StringValue("5.0"), True)
-        )
-        contDesignVars_refProps["lowerBound"].set_value_at(0, VariableState(RealValue(-10.0), True))
-        contDesignVars_refProps["upperBound"].set_value_at(0, VariableState(RealValue(10.0), True))
-        print(f"- continuous design variable bound values set")
+            0, VariableState(StringValue("5.0"), True))
+        contDesignVars_refProps["lowerBound"].set_value_at(
+            0, VariableState(RealValue(-10.0), True))
+        contDesignVars_refProps["upperBound"].set_value_at(
+            0, VariableState(RealValue(10.0), True))
+        print(f'- continuous design variable bound values set')
 
         # run workflow
         print("\nRunning workflow..")
         workflow.run(validation_names={optComp.full_name + ".optimizationToolReturnStatus"})
-
+        
         # get output values and states
-        optToolReturnStatusDatapin = optComp.get_datapins()["optimizationToolReturnStatus"]
+        optToolReturnStatusDatapin = optComp.get_datapins()['optimizationToolReturnStatus']
         if optToolReturnStatusDatapin.get_state().value == 10:
-            print(f"\n- optimization tool return status: Successful Convergence")
+            print(f'\n- optimization tool return status: Successful Convergence')
         else:
-            print(
-                f"\n- optimization tool return status: "
-                f"{optToolReturnStatusDatapin.get_state().value}"
-            )
+            print(f'\n- optimization tool return status: '
+            f'{optToolReturnStatusDatapin.get_state().value}')
 
-        print(
-            f"- new value for {inVarName}: {inDatapin.get_state().value} "
-            f"(valid={inDatapin.get_state().is_valid})"
-        )
-        print(
-            f"- new value for {outVarName}: {outDatapin.get_state().value} "
-            f"(valid={outDatapin.get_state().is_valid})"
-        )
-
+        print(f'- new value for {inVarName}: {inDatapin.get_state().value} '
+            f'(valid={inDatapin.get_state().is_valid})')
+        print(f'- new value for {outVarName}: {outDatapin.get_state().value} ' 
+            f'(valid={outDatapin.get_state().is_valid})')        
+        
         # prompt user if workflow should be saved
         saveInput = input("\nSave workflow (y/n)? ")
         if saveInput in ["yes", "y"]:
@@ -143,10 +133,10 @@ with grpcmc.Engine() as mc:
         elif saveInput in ["no", "n"]:
             print("- workflow will not be saved")
         else:
-            print("Please enter 'y' or 'n'.")  # needs refactor
+            print("Please enter 'y' or 'n'.")   #needs refactor
 
         # pause until user presses any key, then close workflow
-        exitInput = input("\nPress any key to close the workflow and exit..")
+        exitInput = input("\nPress any key to close the workflow and exit..")        
         workflow.close_workflow()
         if saveInput in ["no", "n"]:
             os.remove(workflow_path)
